f12f78c1e52b53cabffc8378795632653f27db78
Thaman Gurung <138950814+ThamanGurung01@users.noreply.github.com>
2026-08-04 11:38:21 +0545

    Update admin user credentials in DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,8 +20,8 @@ class DatabaseSeeder extends Seeder
         User::firstOrCreate(
             ['email' => 'admin@gmail.com'],
             [
-                'name' => 'Test User',
-                'password' => \Illuminate\Support\Facades\Hash::make('password'),
+                'name' => 'Admin',
+                'password' => \Illuminate\Support\Facades\Hash::make('admin@gmail.com'),
             ]
         );
     }
